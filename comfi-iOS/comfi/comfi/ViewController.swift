b744c60e4f9b2baad2030c44e05a4dac4304e517
Brian Li <brian.li.101@gmail.com>
2018-10-13 19:11:03 -0400

    initialization of iOS application

---
+++ comfi-iOS/comfi/comfi/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  comfi
+//
+//  Created by Brian Li on 10/13/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
